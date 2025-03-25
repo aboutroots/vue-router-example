7e25b455fd8002d8a9d54a5bac378db0170fac98
aboutroots <popiel.mw@gmail.com>
2025-03-25 12:31:39 +0100

    add transition

--- src/components/ModalsContainer.vue
+++ src/components/ModalsContainer.vue
@@ -1,23 +1,25 @@
 <template>
-  <div
-    v-if="hasActiveModals"
-    class="fixed inset-0 w-full h-full bg-black bg-opacity-25 flex justify-center items-center z-[1000]"
-  >
-    <transition-group name="fade" tag="div" class="absolute inset-0">
-      <div
-        v-for="(modal, index) in sortedModals"
-        :key="modal.id"
-        class="absolute inset-0 flex justify-center items-center"
-        :style="{ zIndex: String(1000 + index) }"
-      >
-        <component
-          :is="modal.component"
-          v-bind="modal.props"
-          @close="() => closeModal(modal.id)"
-        />
-      </div>
-    </transition-group>
-  </div>
+  <transition name="fade">
+    <div
+      v-if="hasActiveModals"
+      class="fixed inset-0 w-full h-full bg-black bg-opacity-25 flex justify-center items-center z-[1000]"
+    >
+      <transition-group name="fade" tag="div" class="absolute inset-0">
+        <div
+          v-for="(modal, index) in sortedModals"
+          :key="modal.id"
+          class="absolute inset-0 flex justify-center items-center"
+          :style="{ zIndex: String(1000 + index) }"
+        >
+          <component
+            :is="modal.component"
+            v-bind="modal.props"
+            @close="() => closeModal(modal.id)"
+          />
+        </div>
+      </transition-group>
+    </div>
+  </transition>
 </template>
 
 <script lang="ts">
@@ -57,7 +59,7 @@ export default Vue.extend({
   transition: opacity 0.3s ease;
 }
 
-.fade-enter-from,
+.fade-enter,
 .fade-leave-to {
   opacity: 0;
 }
