f064564010da1c88671faf59d91c0b35385f9eb1
aboutroots <popiel.mw@gmail.com>
2025-03-27 11:16:40 +0100

    prepare vercel deployment

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/vue-router-example/" // Replace with your repository name
-      : "/",
+  publicPath: "/",
   // configureWebpack: {
   //   resolve: {
   //     alias: {
